3cbb2ead1280b76ae7c40fefb3c07c7b7cd9cf64
Swathi <swathi.k@indianeagle.com>
2018-11-21 12:50:14 +0530

    modified commit

--- src/main/groovy/com/apress/quickpoll/CustomConfig.groovy
+++ src/main/groovy/com/apress/quickpoll/CustomConfig.groovy
@@ -16,7 +16,7 @@ public class CustomConfig implements WebMvcConfigurer {
         configurer.setSuffix(".html");
         configurer.setTemplateMode(TemplateMode.HTML);
         configurer.setCharacterEncoding("UTF-8");
-        configurer.setOrder(0);  // this is important. This way spring //boot will listen to both places 0 and 1
+        configurer.setOrder(0);  // this is important. This way spring //boot will listen
         configurer.setCheckExistence(true)
         return configurer;
     }
